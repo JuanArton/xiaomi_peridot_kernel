7c98cca34143f1f21cd50cf05cf3041489379927
juanarton <juanarton@protonmail.com>
2025-03-28 10:13:34 +0700

    usb: Import xiaomi changes

--- include/linux/usb/dwc3-msm.h
+++ include/linux/usb/dwc3-msm.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef __LINUX_USB_DWC3_MSM_H
@@ -289,7 +289,7 @@ int msm_ep_clear_ops(struct usb_ep *ep);
 int msm_ep_set_mode(struct usb_ep *ep, enum usb_hw_ep_mode mode);
 int dwc3_core_stop_hw_active_transfers(struct dwc3 *dwc);
 #else
-static inline void dwc3_msm_notify_event(struct dwc3 *dwc,
+void dwc3_msm_notify_event(struct dwc3 *dwc,
 		enum dwc3_notify_event event, unsigned int value)
 { }
 static inline int usb_gsi_ep_op(struct usb_ep *ep, void *op_data,
@@ -310,13 +310,13 @@ static inline bool msm_dwc3_reset_ep_after_lpm(struct usb_gadget *gadget)
 { return false; }
 static inline int dwc3_msm_set_dp_mode(struct device *dev, bool connected, int lanes)
 { return -ENODEV; }
-static inline int msm_ep_update_ops(struct usb_ep *ep)
+int msm_ep_update_ops(struct usb_ep *ep)
 { return -ENODEV; }
-static inline int msm_ep_clear_ops(struct usb_ep *ep)
+int msm_ep_clear_ops(struct usb_ep *ep)
 { return -ENODEV; }
-static inline int msm_ep_set_mode(struct usb_ep *ep, enum usb_hw_ep_mode mode)
+int msm_ep_set_mode(struct usb_ep *ep, enum usb_hw_ep_mode mode)
 { return -ENODEV; }
-static inline int dwc3_core_stop_hw_active_transfers(struct dwc3 *dwc)
+inline int dwc3_core_stop_hw_active_transfers(struct dwc3 *dwc)
 { return 0; }
 #endif
 
@@ -324,9 +324,9 @@ static inline int dwc3_core_stop_hw_active_transfers(struct dwc3 *dwc)
 int dwc3_msm_kretprobe_init(void);
 void dwc3_msm_kretprobe_exit(void);
 #else
-static inline int dwc3_msm_kretprobe_init(void)
+int dwc3_msm_kretprobe_init(void)
 { return 0; }
-static inline void dwc3_msm_kretprobe_exit(void)
+void dwc3_msm_kretprobe_exit(void)
 { }
 #endif
 
